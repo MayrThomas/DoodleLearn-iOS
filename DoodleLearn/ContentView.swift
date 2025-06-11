234dbfb5ab716843bfc05d552227a07ba9bb04c9
thomas.mayr <tmayr1099@gmail.com>
2025-06-11 03:16:08 -0700

    Initial Commit

---
+++ DoodleLearn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DoodleLearn
+//
+//  Created by Thomas Mayr on 11.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DoodleLearn/DoodleLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoodleLearnApp.swift
+//  DoodleLearn
+//
+//  Created by Thomas Mayr on 11.06.25.
+//
+
+import SwiftUI
+
+@main
+struct DoodleLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
